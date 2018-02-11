f58711722ed1f8ff0a2b7b93fda80415f485f3f2
moe091 <moe091@gmail.com>
2018-02-11 01:29:42 -0500

    added pictures and description for nutrack. created an image viewer/picker thing for project displays and made it work with iframes for youtube videos. Started adding pics/videos for merkle brown

--- js/creative.js
+++ js/creative.js
@@ -55,8 +55,56 @@
     scale: 0.3,
     distance: '0px'
   }, 300);
+	
+	
 
-  
+	//Simple custom handler for the image-display section of my project modals. Works automatically for each modal without having to individually set which images belong to which modal and whatnot
+	$('.project-img').click(function(e) {
+		console.log($(e.target).attr('src')); 
+		
+		//check if element has a 'video' data-attribute, if not then set the src of the .project-img-main img in this modal to the source of the image clicked
+		if ($(e.target).data("video") == null) { 
+			//Set main-img src to the src of the clicked img
+			$(e.target).parent().children('.project-img-display').children('.project-img-main').attr('src', $(e.target).attr('src'));
+
+			//Set the href of the link in the main-img to the src of clicked img
+			$(e.target).parent().children('.project-img-display').attr('href', $(e.target).attr('src'));
+
+			//Remove the img-active class from currently active img
+			$(e.target).parent().children('.img-active').removeClass('img-active');
+
+			//Add img-active class to the clicked img
+			$(e.target).addClass('img-active');
+			
+		//if the element DOES have a 'video' data-attribute, then set the iframe in this modals source to the url in the data-video attribute
+		} else { 
+			console.log("setting source of:", $(e.target).parent().children('.project-img-display').children('.project-iframe-wrapper').children('.project-iframe'));
+			console.log("to:", $(e.target).data("video"));
+			
+			//find the project-iframe element, set it's source to the data-video attribute of clicked image
+			$(e.target).parent().children('.project-img-display').children('.project-iframe-wrapper').children('.project-iframe').attr('src', $(e.target).data("video"));
+			
+			//Remove the img-active class from currently active img
+			$(e.target).parent().children('.img-active').removeClass('img-active');
+
+			//Add img-active class to the clicked img
+			$(e.target).addClass('img-active');
+		}
+	});
   
 
 })(jQuery); // End of use strict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
